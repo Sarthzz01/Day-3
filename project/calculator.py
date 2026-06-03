126266bb2c7b640da76e520d023268da824732d2
Sarthzz01 <sarthak31206@gmail.com>
2026-06-03 10:05:59 +0530

    testcases

--- project/calculator.py
+++ project/calculator.py
@@ -1,14 +1,2 @@
-def calculate():
-    print("Select Operation: +, -, *, /")
-    op = input("Enter operator: ")
-    n1 = float(input("First number: "))
-    n2 = float(input("Second number: "))
-
-    if op == '+': print(n1 + n2)
-    elif op == '-': print(n1 - n2)
-    elif op == '*': print(n1 * n2)
-    elif op == '/':
-        print(n1 / n2 if n2 != 0 else "Error: Division by zero")
-    else: print("Invalid Operator")
-
-calculate()
+def add(a,b):
+    return a+b
